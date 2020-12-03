15c6940916b731caacaa675b135a1b62b83ddf33
RJPenic <rafael-josip.penic@fer.hr>
2020-12-03 17:23:58 +0100

    Added Hierarchical Attention Network model template

---
+++ orpheus/han_model.py
@@ -0,0 +1,29 @@
+import torch
+from torch import nn
+
+class HAN_Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        #TODO
+
+    def forward(self, x):
+    	#TODO
+        pass
+
+class GRU_Net(nn.Module):
+	def __init__(self):
+        super().__init__()
+        #TODO
+
+    def forward(self, x):
+    	#TODO
+        pass
+
+class Attention(nn.Module):
+	def __init__(self):
+        super().__init__()
+        #TODO
+
+    def forward(self, x):
+    	#TODO
+        pass
