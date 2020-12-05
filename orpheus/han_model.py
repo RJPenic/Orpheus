44ca0e1718e0088729bff16d98727a0149d898de
RJPenic <rafael-josip.penic@fer.hr>
2020-12-05 14:57:50 +0100

    Fixed a bug that was causing invalid output dimensions, added model arguments

--- orpheus/han_model.py
+++ orpheus/han_model.py
@@ -3,27 +3,52 @@ from torch import nn
 import torch.nn.functional as F
 
 import dataset as dl
+from argparse import ArgumentParser
 
 class HAN_Model(nn.Module):
     def __init__(self,
             vocab,
-            input_dim = 300,
+            input_dim = 100,
             word_hidden_dim = 50,
             line_hidden_dim = 50,
             word_att_layers = 1,
             line_att_layers = 1,
-            word_droput = 0.5,
+            word_dropout = 0.5,
             line_dropout = 0.5,
             pretrain_file = None,
             num_classes = 10):
         super().__init__()
-        
-        self.word_att = Attention(input_dim, word_hidden_dim, word_att_layers, word_droput)
+        self.encoder = dl.load_vec_repr(vocab, d = input_dim, file = pretrain_file, freeze = pretrain_file is not None)
+
+        self.word_att = Attention(input_dim, word_hidden_dim, word_att_layers, word_dropout)
         self.line_att = Attention(2 * word_hidden_dim, line_hidden_dim, line_att_layers, line_dropout)
 
         self.final_proj = nn.Linear(2 * line_hidden_dim, num_classes)
 
-        self.encoder = dl.load_vec_repr(vocab, file = pretrain_file, freeze = pretrain_file is not None)
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = ArgumentParser(parents = [parent_parser], add_help = False)
+
+        parser.add_argument('--pretrain_file', type=str,
+                        help="Path to file containing word vector representations")
+        parser.add_argument('--word_hidden_dim', type=int, default = 50,
+                        help="Size of hidden states in word attention module")
+        parser.add_argument('--line_hidden_dim', type=int, default = 50,
+                        help="Size of hidden states in line attention module")
+        parser.add_argument('--word_att_layers', type=int, default = 1,
+                        help="Number of layers used in word attention module")
+        parser.add_argument('--line_att_layers', type=int, default = 1,
+                        help="Number of layers used in line attention module")
+        parser.add_argument('--word_dropout', type=float, default = 0.5,
+                        help="Probability of dropout on the output of word attention module")
+        parser.add_argument('--line_dropout', type=float, default = 0.5,
+                        help="Probability of dropout on the output of line attention module")
+        parser.add_argument('--num_classes', type=int, default = 10,
+                        help="Number of possible classes in dataset")
+        parser.add_argument('--input_dim', type=int, default = 100,
+                        help="Dimension of model inputs")
+
+        return parser
 
     def forward(self, x):
         x = x.permute(1, 0, 2)
@@ -36,6 +61,7 @@ class HAN_Model(nn.Module):
 
         x = torch.cat(temp, 1)
         x = self.line_att(x)
+        x = x.squeeze(1)
 
         x = self.final_proj(x)
 
