bd9740fe64cff3464e41a0b9f6089fd1acf5b97f
RJPenic <rafael-josip.penic@fer.hr>
2020-12-05 14:58:49 +0100

    Refactoring

--- orpheus/dataset.py
+++ orpheus/dataset.py
@@ -9,6 +9,8 @@ import csv
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
+import han_model
+
 @dataclass
 class Instance:
     label : int
@@ -19,38 +21,43 @@ class Instance:
         self.text = text
 
 class LyricsDataset(torch.utils.data.Dataset):
-    def __init__(self, instances, max_vocab_size = 33000, max_lines = 30, max_words_per_line = 10, remove_stop_words = False):
+    def __init__(self, instances, max_vocab_size = 30000, max_lines = 30, max_words_per_line = 10, remove_stop_words = False):
         self.instances = instances
-        self.stop_words = stopwords.words('english')
-        self.stopwords.update(['you\'re', 'i\'m', 'she\'s', 'he\'s', 'it\'s'])
+        self.stop_words = set(stopwords.words('english'))
+        self.stop_words.update(['you\'re', 'i\'m', 'she\'s', 'he\'s', 'it\'s', '\'re', '\'m', '\'s'])
 
         ct_txt = {}
 
         for instance in instances:
             for line in instance.text:
                 for token in line:
-                    if remove_stop_words and token in self.stopwords:
+                    if not (remove_stop_words and token in self.stop_words):
                         ct_txt[token] = ct_txt.get(token, 0) + 1
 
-        self.text_vocab = Vocab(ct_txt, max_lines, max_words_per_line, max_vocab_size)
+        self.text_vocab = Vocab(ct_txt, max_lines, max_words_per_line, max_size = max_vocab_size)
         
-    def from_file(filename, labels, max_lines = 30, max_words_per_line = 10, skip_first_line = True, remove_stop_words = False):
+    def from_file(filename, max_lines = 30, max_words_per_line = 10, skip_first_line = True, remove_stop_words = False, max_vocab_size = 30000):
         instances = []
-        labels = [label.lower() for label in labels]
+        labels = []
 
         with open(filename) as csvfile:
             csv_reader = csv.reader(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
             for i, row in enumerate(csv_reader):
+                print(i)
                 if i == 0 and skip_first_line:
                     continue
 
+                label = row[5].lower()
+                if label not in labels:
+                    labels.append(label)
+
                 instances.append(Instance(
-                    int(labels.index(row[5].lower())),
-                    [word_tokenize(line) for line in row[6].split('\n')]
+                    int(labels.index(label)),
+                    [line.split() for line in row[6].split('\n')[:max_lines]]
                 ))
 
-        return LyricsDataset(instances, len(labels), max_lines, max_words_per_line, remove_stop_words)
+        return LyricsDataset(instances, max_vocab_size, max_lines, max_words_per_line, remove_stop_words)
 
     def __getitem__(self, i):
         return self.text_vocab.encode(self.instances[i].text), self.instances[i].label
@@ -62,7 +69,6 @@ class Vocab:
     def __init__(self, frequencies, max_lines, max_words_per_line, max_size = -1, min_freq = 0,
                 special = ["<PAD>", "<UNK>"]): # maybe add additional special for line padding ???
         self.stoi = {}
-
         self.max_lines = max_lines
         self.max_words_per_line = max_words_per_line
 
@@ -72,8 +78,9 @@ class Vocab:
         sorted_tokens = sorted(frequencies.keys(), key = lambda k: -frequencies[k])
 
         for t in sorted_tokens:
-            if min_freq <= frequencies[t] or (len(self.stoi) < max_size or max_size <= 0):
-                self.stoi[t] = len(self.stoi)
+            if min_freq > frequencies[t] or (len(self.stoi) >= max_size and max_size != -1) :
+                break
+            self.stoi[t.lower()] = len(self.stoi)
 
     def encode(self, text):
         encoded = []
@@ -87,7 +94,7 @@ class Vocab:
                 if i >= self.max_words_per_line:
                     break
 
-                temp.append(self.stoi.get(token, self.stoi["<UNK>"]))
+                temp.append(self.stoi.get(token.lower(), self.stoi["<UNK>"]))
             
             encoded.append(temp)
 
@@ -100,7 +107,7 @@ def load_vec_file_to_dict(filename):
     content = [x.strip() for x in content]
     
     vecs = {}
-    
+
     for line in content:
         elems = line.split()
         vecs[elems[0]] = torch.Tensor([float(n) for n in elems[1:]])
@@ -142,21 +149,21 @@ def pad_collate_fn(batch, pad_index = 0): # ???
             line_len = nums_words[i][j]
             texts_tensor[i, j, :line_len] = torch.LongTensor(line)
 
-    return texts_tensor, torch.LongTensor(labels), torch.LongTensor(nums_lines), line_lens_tensor
+    return texts_tensor, torch.LongTensor(labels)
 
 # ------ ˇˇˇ IGNORE ˇˇˇ ------
 if __name__ == "__main__":
     filepath = '../dataset/lyrics_1.csv'
 
-    ds = LyricsDataset.from_file(filepath, ["Rock", "Pop", "Hip-Hop", "Metal", "Country", "Jazz", "Electronic", "R&B", "Indie", "Folk", "Other"])
-    print(ds[0])
+    ds = LyricsDataset.from_file(filepath)
+    train_dataloader = DataLoader(dataset=ds, batch_size=4, 
+                                  shuffle=False, collate_fn=pad_collate_fn, num_workers = 4)
 
-    train_dataloader = DataLoader(dataset=ds, batch_size=2, 
-                                  shuffle=False, collate_fn=pad_collate_fn)
+    model = han_model.HAN_Model(ds.text_vocab)
 
-    for text, labels, len1, len2 in train_dataloader:
+    for text, labels in train_dataloader:
         print(text)
+        print(text.shape)
         print(labels)
-        print(len1)
-        print(len2)
+        model(text)
         break
